ecb093e8027195bca3878b0c05550ebea751a600
kardinar <karimovdinar@gmail.com>
2022-05-16 22:54:34 +0300

    update StrAddInt

--- lesson3/StrAddInt.go
+++ lesson3/StrAddInt.go
@@ -10,6 +10,12 @@ func StrAddInt(first string, second int) string {
 	return (first + secondString)
 }
 
+func StrAddInt2(first string, second int) string {
+	secondString := fmt.Sprint(second)
+	return (first + secondString)
+}
+
 func main() {
 	fmt.Printf(StrAddInt("Go to the Moon ", 3000))
+	fmt.Printf(StrAddInt2("\nGo to the Moon ", 3000))
 }
