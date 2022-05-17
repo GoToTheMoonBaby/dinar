85ada15c3d3bfdd9534fb29cf3fcf5f03ec36390
kardinar <karimovdinar@gmail.com>
2022-05-17 16:50:54 +0300

    create StrSub

---
+++ lesson3/StrSub.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func StrSub(first, second string) bool {
+	var flag = false
+	if len(second) > len(first) {
+		return false
+	}
+	for i := 0; i < len(first); i++ {
+		if first[i] == second[0] {
+			for j := 1; j < len(second); j++ {
+				if (i+j < len(first)) && (first[i+j] == second[j]) {
+					flag = true
+				} else {
+					flag = false
+				}
+			}
+			if flag == true {
+				return flag
+			}
+		}
+	}
+	return flag
+}
+
+func main() {
+	res := StrSub("Go to the Moon", "Moon")
+	//st := string(res)
+	//	fmt.Printf(st)
+	fmt.Printf("%v", res)
+}
